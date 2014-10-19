fc8954e1ae5da9b587e4dab204becd5ee565aa5a
Jon Whelan <jon.m.whelan@gmail.com>
2014-10-19 15:54:01 -0700

    Change private property name

--- src/Maps/ArrayCacheMap.php
+++ src/Maps/ArrayCacheMap.php
@@ -8,37 +8,37 @@ class ArrayCacheMap implements CacheMapInterface
 {
 	use MapGet;
 
-	private $cachedData;
+	private $cache;
 
-	public function __construct(array $cachedData = [])
+	public function __construct(array $cache = [])
 	{
-		$this->cachedData = $cachedData;
+		$this->cache = $cache;
 	}
 
 	public function find($key)
 	{
-		return array_key_exists($key, $this->cachedData)
-			? new \ArrayIterator([$this->cachedData[$key]])
+		return array_key_exists($key, $this->cache)
+			? new \ArrayIterator([$this->cache[$key]])
 			: new \EmptyIterator;
 	}
 
 	public function set($key, $value)
 	{
-		$this->cachedData[$key] = $value;
+		$this->cache[$key] = $value;
 	}
 
 	public function contains($key)
 	{
-		return array_key_exists($key, $this->cachedData);
+		return array_key_exists($key, $this->cache);
 	}
 
 	public function clear()
 	{
-		$this->cachedData = [];
+		$this->cache = [];
 	}
 
 	public function remove($key)
 	{
-		unset($this->cachedData[$key]);
+		unset($this->cache[$key]);
 	}
 }
